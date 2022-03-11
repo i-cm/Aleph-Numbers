f505d36e567aa8a1ce0d837dc57f098cacb90f35
i-cm <74533816+i-cm@users.noreply.github.com>
2022-03-11 18:06:39 +1100

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 player={lastTick:0,
         Numbers:0,
         Infinity:{amount:0,
-                  unlocked:0},
+                  unlocked:false},
         Version:{MajorUpdate:0,
                  MinorUpdate:0,
                  VeryMinorUpdate:0}
@@ -20,8 +20,21 @@ function add() {
   render()
 }
 
+function infinity() {
+  player.Numbers=0
+  player.Infinity.amount+=1
+  player.Infinity.unlocked=true
+  render()
+}
+
 function render() {
   document.getElementById('num').textContent=player.Numbers
+  document.getElementById('inf').textContent=player.Infinity.amount
+  if (player.Infinity.unlocked) {
+    show('infunlockable')
+  } else {
+    hide('infunlockable')
+  }
 }
 
 render()
